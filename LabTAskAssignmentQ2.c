dc4c58312c7363f2414cf8a1ce92bd1d14c3513e
Samarth101 <bhandegaonkarsamarth2@gmail.com>
2024-09-10 18:32:29 +0530

    Delete LabTAskAssignmentQ2.c

--- LabTAskAssignmentQ2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int dist;
-    printf("Enter value for Distance in Inches:-\n");
-    scanf("%d",&dist);
-    printf("Distance in Cms is %0.2f\n",dist*2.54);
-    return 0;
-}

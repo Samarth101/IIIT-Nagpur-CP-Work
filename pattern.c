9ab118fe832d185cb93d4b81144a597411c77306
Samarth101 <bhandegaonkarsamarth2@gmail.com>
2024-09-16 13:14:15 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -707,35 +707,7 @@
 
 
 //Pascal Traingle
-#include<stdio.h>
-int main()
-{
-    int num;
-    printf("Enter Number of rows for pascal triangle =\n");
-    scanf("%d",&num);
-    for (int i = 1; i <=num; i++)
-    {
-        for (int j = 1; j <=num-i; j++)
-        {
-            printf(" ");
-        }
-        for (int k = 1; k <=(2*i-1); k++)
-        {
-            if (k==1 || k==(2*i-1))
-            {
-                printf("%d",1);
-            }
-            else if (k%2==0)
-            {
-                printf(" ");
-            }
-            else
-                printf("%d",i-1);
-        }
-        printf("\n");
-    }
-    return 0;
-}
+
 
 
 

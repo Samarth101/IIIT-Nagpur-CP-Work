38c3a2ffd30ffddc9dee3d38e1bde10ceba59fd0
Samarth101 <bhandegaonkarsamarth2@gmail.com>
2024-11-25 16:08:14 +0530

    Delete LabTaskAssignmentQ6.c

--- LabTaskAssignmentQ6.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    char ch;
-    printf("Enter any character:- ");
-    scanf("%c",&ch);
-    printf("The ASCII Value for Character %c = %d\n",ch,ch);
-    return 0;
-}
